2e29c44985035cf3e628b80a0522eafc9907aa63
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-09-09 21:08:45 +0200

    add navigation

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Countdown = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <h3>Countdown Component</h3>
+            </div>
+        )
+    }
+})
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Timer = React.createClass({
+    render: function (){
+        return (
+            <div>
+                <h3>Timer Component</h3>
+            </div>
+        )
+    }
+})
+
+module.exports = Timer;
