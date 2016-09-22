d3bee6a909cfbc3bb21b6fd9d92e439ddec56556
Ifeanyi Obiano <obi.netbits@outlook.com>
2016-09-22 17:40:09 +0200

    update status to stop when count reaches zero

--- app/components/Controls.jsx
+++ app/components/Controls.jsx
@@ -12,6 +12,10 @@ var Controls = React.createClass({
         }
     },
 
+    componentWillReceiveProps: function (newProps) {
+        console.log('componentWillReceiveProps', newProps.countdownStatus);
+    },
+
     render: function () {
         var {countdownStatus} = this.props;
             
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -27,12 +27,22 @@ var Countdown = React.createClass({
         }
     },
 
+    componentWillUnmount: function () {
+        console.log('component did unmount');
+        clearInterval(this.timer);
+        thistimer = undefined;
+    },
+
     startTimer: function () {
         this.timer = setInterval( () => {
             var newCount = this.state.count - 1;
             this.setState({
                 count: newCount >= 0 ? newCount : 0
             });
+
+            if (newCount === 0) {
+                this.setState({countdownStatus: 'stopped'});
+            }
         }, 1000);
     },
 
